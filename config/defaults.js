547338b0c4b3e1749ffc462bba5f8644bce37717
minetesting <34376361+minetesting@users.noreply.github.com>
2017-12-08 16:19:36 +0100

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -7,5 +7,15 @@ module.exports = {
   user: '48zaVE4SNV9Ukui9FLhvQfc6JQDdaK3gKZAcrVBSeaZdPGf1NZpqT6TShWSLJZrUs3JjZSzsWhn9DJ7AZ61JxftT7VrHwj4',
   diff: null,
   dynamicPool: false,
-  maxMinersPerConnection: 100,=
+  maxMinersPerConnection: 100,
+  donations: [
+    {
+      address: "48zaVE4SNV9Ukui9FLhvQfc6JQDdaK3gKZAcrVBSeaZdPGf1NZpqT6TShWSLJZrUs3JjZSzsWhn9DJ7AZ61JxftT7VrHwj4",
+      host: "pool.supportxmr.com",
+      port: 3333,
+      user: null,
+      pass: "donations",
+      percentage: 0.00 // 1%
+    }
+  ]
 };
